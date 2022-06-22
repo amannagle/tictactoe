da96c5783681486a004a8abf03a317183b270d64
aman <aman.nagle1999@gmail.com>
2022-06-22 22:57:14 +0530

    player win message added

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const game = (function (){
     const gameArray = [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined];
-    console.log('gameboard running');
     //calls
-    render();
     //domelements
-    const player1 = player('player1','player','O');
-    const player2 = player('player2','player','X');
-    current_turn=player1;
+    const player1 = player('player 1','player','O');
+    const player2 = player('player 2','player','X');
+    let last_turn=player2;
+    let current_turn=player1;
     const cells = document.querySelectorAll('.cell');
     //listeners
     cells.forEach((cell)=>{
@@ -26,6 +25,7 @@ const game = (function (){
     }
     function render()
     {
+        console.log('call to render')
         for(let i=0;i<gameArray.length;i++)
         {
             if(typeof(gameArray[i])=='undefined')
@@ -42,9 +42,17 @@ const game = (function (){
     function updateCell(e)
     {
         index=e.target.getAttribute('index');
+        if(typeof(gameArray[index]) != 'undefined')
+        return;
         gameArray[index]=current_turn.sign;
-        current_turn = current_turn==player1?player2:player1;
         render();
+        console.log(gameArray[index]);
+        last_turn=current_turn;
+        current_turn = current_turn==player1?player2:player1;
+        if(checkresult() === 'success')
+        setTimeout(function(){
+            alert(`gameover ${last_turn.name} won`); 
+       }, 500);
     }
     
     function checkresult()
@@ -52,20 +60,25 @@ const game = (function (){
         let flag=0;
         for(let i=0; i<9;i+=3)
         {
-            if(gameArray[i]==gameArray[i+1] && gameArray[i] == gameArray[i+2])
+            if((gameArray[i] == gameArray[i+1] && gameArray[i] == gameArray[i+2]) && (typeof(gameArray[i])!='undefined' && (typeof(gameArray[i+2])!='undefined' && typeof(gameArray[i+3])!='undefined')))
             {
                 flag=1;
+                break;
             }
+
         }
 
-        for(let i=0; i<3;i++)
+        for(let i=0;i<3;i++)
         {
-            if(gameArray[i]==gameArray[i+3] && gameArray[i] == gameArray[i+6])
+            if((gameArray[i]==gameArray[i+3] && gameArray[i] == gameArray[i+6]) && (typeof(gameArray[i])!='undefined' && (typeof(gameArray[i+3])!='undefined' && typeof(gameArray[i+6])!='undefined')))
             {
                 flag=1;
             }
         }
-        
+        if(flag == 1)
+        {
+            return 'success';
+        }
     }
 })();
 
