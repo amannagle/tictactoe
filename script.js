9b3d2bc517608160f5414899805f97b341e539d6
aman <aman.nagle1999@gmail.com>
2022-06-24 00:22:36 +0530

    added score

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const game = (function (){
     let gameArray = [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined];
     //calls
     //domelements
-    const player1 = player('player 1','player','O');
-    const player2 = player('player 2','player','X');
+    const player1 = player('player 1','player','O',0);
+    const player2 = player('player 2','player','X',0);
     let last_turn=player2;
     let current_turn=player1;
     const start_button = document.querySelector('#game-start');
     const cells = document.querySelectorAll('.cell');
     const board = document.querySelector('.board');
+    const main = document.querySelector('main');
+    const header = document.querySelector('header');
+    const score = document.querySelector('.score');
+    const player1_score = document.querySelector('#player1score');
+    const player2_score = document.querySelector('#player2score');
     const select_player_div = document.querySelector('.select-player');
     //listeners
     start_button.addEventListener('click',gameStart);
@@ -23,22 +28,27 @@ const game = (function (){
         const opponent = player_select.value;
         player2.type=opponent;
         opponent=='ai'?player2.name='AI':player2.name='player2';
-        console.log(player2);
         render();
     }
     function searchCell(index)
     {
         return (document.querySelector(`div[index='${index}']`));
     }
-    function player(name,type,sign)
+    function player(name,type,sign,score)
     {
         
-        return{name: name,type: type,sign: sign};
+        return{name: name,type: type,sign: sign,score:score};
     }
     function render()
     {
+        checkGame();
         board.style['display']='grid';
         select_player_div.style['display']='none';
+        header.style['display']='none';
+        main.style['display']='flex';
+        score.style['display']='block';
+        player1_score.textContent=player1.score;
+        player2_score.textContent=player2.score;
         for(let i=0;i<gameArray.length;i++)
         {
             if(typeof(gameArray[i])=='undefined')
@@ -51,7 +61,14 @@ const game = (function (){
             }
         }
     }
-
+    function checkGame()
+    {
+        if(player1.score == 3 || player2.score ==3)
+        {
+            let winner = player1.score>player2.score?player1.name:player2.name;
+            alert(`game is over ${winner} won`)
+        }
+    }
     function updateCell(e)
     {
         index=e.target.getAttribute('index');
@@ -112,8 +129,6 @@ const game = (function (){
                 result = 'success';
             }
         }
-        for(let i=0; i<2;i+=2)
-        
         if((gameArray[0]==gameArray[4] && gameArray[0] == gameArray[8]) && (typeof(gameArray[0])!='undefined' && (typeof(gameArray[4])!='undefined' && typeof(gameArray[8])!='undefined')))
             {
                 result = 'success';
@@ -131,7 +146,10 @@ const game = (function (){
         }
         if(result == 'success')
         {
+            last_turn.score++;
             alert(`${last_turn.name} won`);
+            last_turn=current_turn;
+            current_turn = current_turn==player1?player2:player1;
             gameArray=[undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined];
             render();
         }
